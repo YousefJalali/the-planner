fd7fc8a55d906274d821d01b03bf5889de650c5f
Yousef Jalali <yousefjalali@yousefs-MacBook-Pro.local>
2022-05-18 15:04:35 +0300

    fix date tasks fetch

--- pages/api/tasks/index.ts
+++ pages/api/tasks/index.ts
@@ -16,15 +16,24 @@ const handler = async (
   }
 
   const startDate = new Date(d)
+  startDate.setHours(0, 0, 0, 0)
+  console.log('startDate', startDate)
+
+  // 2022-05-18T21:00:00.000Z
+  // 2022-03-31T21:00:00.000+00:00
 
   try {
     const tasks = await prisma.task.findMany({
-      // where: { startDate },
-      take: 5,
+      where: {
+        startDate: {
+          lte: startDate,
+        },
+      },
+      // take: 5,
       include: { project: { select: { title: true, color: true } } },
     })
 
-    console.log('prisma req', tasks)
+    // console.log('prisma req', tasks)
 
     return res.status(200).json({ data: tasks })
   } catch (error) {
