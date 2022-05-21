d7e640b34ce6ac1423bc2e074902c728e50b2749
Yousef Jalali <yousefjalali@yousefs-MacBook-Pro.local>
2022-05-21 19:24:48 +0300

    add scroll snap

--- components/ScrollableList.tsx
+++ components/ScrollableList.tsx
@@ -4,11 +4,18 @@ const ScrollableList = styled(x.ul)`
   display: flex;
   padding: 0 4;
   overflow-x: scroll;
+  scroll-snap-type: x mandatory;
 
   -ms-overflow-style: none; /* for Internet Explorer, Edge */
   scrollbar-width: none; /* for Firefox */
   &::-webkit-scrollbar {
     display: none; /* for Chrome, Safari, and Opera */
+  }
+
+  > li,
+  > div {
+    scroll-behavior: smooth;
+    scroll-snap-align: center;
   }
 
   &::after {
