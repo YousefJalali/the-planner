88cceafb0e053e7f85888f343fb2278d3e08fbbc
Yousef Jalali <yousefjalali@yousefs-MacBook-Pro.local>
2022-08-12 11:07:00 +0300

    fix view

--- apps/web/components/search/search-input.tsx
+++ apps/web/components/search/search-input.tsx
@@ -1,23 +1,33 @@
 import { Fieldset, Input } from '@the-planner/ui-web'
+import Head from 'next/head'
 import { InputHTMLAttributes } from 'react'
 import { FiSearch } from 'react-icons/fi'
 
 const SearchInput = (props: InputHTMLAttributes<HTMLInputElement>) => {
   return (
-    <Fieldset
-      label="search"
-      hideLabel
-      supportiveText="type a word from title or description"
-      leftIcon={<FiSearch />}
-    >
-      <Input
-        {...props}
-        type="search"
-        name="keyword"
-        placeholder="Search..."
-        autoComplete="off"
-      />
-    </Fieldset>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        ></meta>
+      </Head>
+
+      <Fieldset
+        label="search"
+        hideLabel
+        supportiveText="type a word from title or description"
+        leftIcon={<FiSearch />}
+      >
+        <Input
+          {...props}
+          type="search"
+          name="keyword"
+          placeholder="Search..."
+          autoComplete="off"
+        />
+      </Fieldset>
+    </>
   )
 }
 
--- apps/web/components/search/search-page.tsx
+++ apps/web/components/search/search-page.tsx
@@ -1,5 +1,5 @@
 import { x } from '@xstyled/styled-components'
-import Head from 'next/head'
+
 import { useState } from 'react'
 
 import { useSearch } from '@the-planner/data'
@@ -20,56 +20,47 @@ export const SearchPage = () => {
   const { handler } = useSearchHistory()
 
   return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        ></meta>
-      </Head>
+    <x.main h="100vh" pb={4}>
+      <x.section px={4}>
+        <x.span
+          text="body.large"
+          display="block"
+          w="calc(100% - 48px)"
+          lineHeight="normal"
+        >
+          What task or project are you looking for?
+        </x.span>
 
-      <x.main h="100vh" pb={4}>
-        <x.section px={4}>
-          <x.span
-            text="body.large"
-            display="block"
-            w="calc(100% - 48px)"
-            lineHeight="normal"
-          >
-            What task or project are you looking for?
-          </x.span>
+        <SearchInput
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+          autoFocus
+          onKeyDown={(e) => handler(e)}
+        />
+      </x.section>
 
-          <SearchInput
-            value={val}
-            onChange={(e) => setVal(e.target.value)}
-            autoFocus
-            onKeyDown={(e) => handler(e)}
-          />
+      {val.length <= 0 ? (
+        <>
+          <SearchHistory onSearchItemClick={setVal} />
+          <RecentTasks />
+        </>
+      ) : isLoading ? (
+        <x.div m="0 auto" w="fit-content" my={3}>
+          <SearchLoading />
+        </x.div>
+      ) : searchedTasks?.length > 0 ? (
+        <x.section p={3} h="100%" flex="1 1 auto">
+          <SearchList data={searchedTasks} />
         </x.section>
-
-        {val.length <= 0 ? (
-          <>
-            <SearchHistory onSearchItemClick={setVal} />
-            <RecentTasks />
-          </>
-        ) : isLoading ? (
-          <x.div m="0 auto" w="fit-content" my={3}>
-            <SearchLoading />
-          </x.div>
-        ) : searchedTasks?.length > 0 ? (
-          <x.section p={3} h="100%" flex="1 1 auto">
-            <SearchList data={searchedTasks} />
-          </x.section>
-        ) : (
-          <EmptyState
-            illustration={<NoSearchDataSvg />}
-            title=" No data found"
-            description="Try other words"
-            size="20%"
-          />
-        )}
-      </x.main>
-    </>
+      ) : (
+        <EmptyState
+          illustration={<NoSearchDataSvg />}
+          title=" No data found"
+          description="Try other words"
+          size="20%"
+        />
+      )}
+    </x.main>
   )
 }
 
