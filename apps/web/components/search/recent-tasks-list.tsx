325ed4af869975083b0cdcf32bddc8ec171fe32f
Jbygoal <jbtgoal@gmail.com>
2023-05-23 19:30:16 +0300

    enhance empty state

--- apps/web/components/search/recent-tasks-list.tsx
+++ apps/web/components/search/recent-tasks-list.tsx
@@ -3,6 +3,7 @@ import { SearchedTask } from '../task/task-item'
 import ViewTask from '../task/view-task'
 import TaskItemSearchPlaceholder from '../task/task-item/task-item-search-placeholder'
 import Skeleton from 'react-loading-skeleton'
+import { RiKeyLine } from 'react-icons/ri'
 
 const RecentTasks = () => {
   const { tasks, isLoading } = useTasks({
@@ -26,31 +27,32 @@ const RecentTasks = () => {
             </ul>
           </section>
         </>
+      ) : tasks && tasks.length > 0 ? (
+        <>
+          <h3>Recent tasks</h3>
+          <section className="not-prose">
+            <ul className="space-y-3 pb-6">
+              {tasks.map((task) => (
+                <li key={task.id}>
+                  <ViewTask task={task}>
+                    {(showModal) => (
+                      <button onClick={showModal} className="w-full text-left">
+                        <SearchedTask task={task} />
+                      </button>
+                    )}
+                  </ViewTask>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </>
       ) : (
-        tasks &&
-        tasks.length > 0 && (
-          <>
-            <h3>Recent tasks</h3>
-            <section className="not-prose">
-              <ul className="space-y-3 pb-6">
-                {tasks.map((task) => (
-                  <li key={task.id}>
-                    <ViewTask task={task}>
-                      {(showModal) => (
-                        <button
-                          onClick={showModal}
-                          className="w-full text-left"
-                        >
-                          <SearchedTask task={task} />
-                        </button>
-                      )}
-                    </ViewTask>
-                  </li>
-                ))}
-              </ul>
-            </section>
-          </>
-        )
+        <p className="text-center pt-4">
+          <RiKeyLine size={48} className="mx-auto mb-2" />
+          While you may not have any tasks yet, creating an account and starting
+          to add tasks will empower you to seize control of your time, set clear
+          goals, celebrate your progress, stay organized, and reduce stress.
+        </p>
       )}
     </section>
   )
